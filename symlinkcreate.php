c094c87a97eaacca7d395ab115fca96b8bc6d200
Arturas Bespalovas <artazzs1@gmail.com>
2018-12-03 10:33:16 +0200

    avatars fixed

---
+++ symlinkcreate.php
@@ -0,0 +1,9 @@
+<?php
+//symlink('/home/u140454520/domains/arturasdesign.xyz/app/public/uploads/avatars', '/home/u140454520/domains/arturasdesign.xyz/public_html/uploads/avatars');
+exec('/home/u140454520/domains/arturasdesign.xyz/app/public/uploads/avatars', '/home/u140454520/domains/arturasdesign.xyz/public_html/uploads/avatars');
+
+
+ln -s /under_root/somefolder/ /home/deploy/.BuildServer
+    
+    
+    ln -s /home/u140454520/domains/arturasdesign.xyz/app/public/uploads/ /home/u140454520/domains/arturasdesign.xyz/public_html
